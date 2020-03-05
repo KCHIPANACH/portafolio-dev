050c2e4087c75ba8c53c8593c1edefd39520af71
Kevin Chipana <kvn.chipana@gmail.com>
2020-03-05 07:36:03 -0500

    Hasta skills con el pull de distitna historia de master

--- src/containers/Skill.jsx
+++ src/containers/Skill.jsx
@@ -26,7 +26,7 @@ const Skill = ()=>{
                     </div>
                     <div className="contenedor_parrafo">
                         <p className="m_0 font_ubuntu f_h6 c_white f_100">
-                            Me considero una persona con un alto deseo o hambre de crear o emprender algo, solucionar algo en especifico <br /> <br />
+                            Me considero una persona con un alto deseo o hambre de crear o emprender algo, solucionar algo en especifico. <br /> <br />
                             Tengo experiencia en el mundo WEB, he realizado multiples proyectos desde Landing Pages hasta Sistemas o Dashboard informaticos, domino tanto tecnologías frontend como backend por lo que me he desempeñado como full-stack developer. <br /> <br />
                             Actualmente me encuentro junto a unos amigos realizando "HOPE" una plataforma que unificará todas las casas de adopción de mascotas abandonadas, tambien me encuentro aprendiendo diariamente nuevas tecnologías, ademas de ingles y como meta personal de este año deseo aprender IA y terminar HOPE. 
                         </p>
